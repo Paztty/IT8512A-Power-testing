0f36c6aa2a44ec104fa26b84a61a0e8a7f9b90c5
HVTan <Hovantan1107@gmail.com>
2020-08-04 10:44:20 +0700

    able to read data

--- 2.Software/IT8512A Power Test/Form1.Designer.cs
+++ 2.Software/IT8512A Power Test/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace IT8512A_Power_Test
+﻿using System.Windows.Forms;
+
+namespace IT8512A_Power_Test
 {
     partial class mainForm
     {
@@ -29,21 +31,59 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textDataResponse = new System.Windows.Forms.TextBox();
+            this.PortLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 186);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test";
+            // 
+            // textDataResponse
+            // 
+            this.textDataResponse.Location = new System.Drawing.Point(68, 189);
+            this.textDataResponse.Name = "textDataResponse";
+            this.textDataResponse.Size = new System.Drawing.Size(525, 20);
+            this.textDataResponse.TabIndex = 1;
+            // 
+            // PortLabel
+            // 
+            this.PortLabel.AutoSize = true;
+            this.PortLabel.Location = new System.Drawing.Point(610, 223);
+            this.PortLabel.Name = "PortLabel";
+            this.PortLabel.Size = new System.Drawing.Size(35, 13);
+            this.PortLabel.TabIndex = 2;
+            this.PortLabel.Text = "label1";
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PortLabel);
+            this.Controls.Add(this.textDataResponse);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "mainForm";
             this.Text = "IT8512A Power Test";
+            this.Load += new System.EventHandler(this.mainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.TextBox textDataResponse;
+        public Label PortLabel;
+        private Button button1;
     }
 }
 
--- 2.Software/IT8512A Power Test/Form1.cs
+++ 2.Software/IT8512A Power Test/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* IT8512A test machine */
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +11,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// add for Serial port
+using System.IO;
+using System.IO.Ports;
+using System.Xml;
+
+
+
 
 namespace IT8512A_Power_Test
 {
     public partial class mainForm : Form
     {
+        SerialPort serialPort1 = new SerialPort();
+        delegate void SetTextCallback(String data);
+        String InputData = String.Empty;
+        String[] PortName = { };
+
         public mainForm()
         {
             InitializeComponent();
+            PortName = SerialPort.GetPortNames();
+            serialPort1.PortName = PortName[0];
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = Parity.None;
+
+
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceive);
         }
+        private void DataReceive(object obj, SerialDataReceivedEventArgs e)
+        {
+            InputData = serialPort1.ReadExisting();
+            if (InputData != String.Empty)
+            {
+                SetText(InputData); // Chính vì vậy phải sử dụng ủy quyền tại đây. Gọi delegate đã khai báo trước đó
+            }
+        }
+        private void SetText(string text)
+        {
+            if (this.textDataResponse.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText); // khởi tạo 1 delegate mới gọi đến SetText
+                this.Invoke(d, new object[] { text });
+            }
+            else this.textDataResponse.Text += text;
+        }
+
+         private void mainForm_Load(object sender, EventArgs e)
+         {
+            serialPort1.Open();
+            PortLabel.Text = PortName[0];
+        }
+
     }
 }
